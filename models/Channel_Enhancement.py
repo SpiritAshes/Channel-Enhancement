16dbd17e48be8974de6dc34594e28c9956eb3640
SpiritAshes <92069795+SpiritAshes@users.noreply.github.com>
2023-04-03 15:09:39 +0800

    Update Channel_Enhancement.py

--- models/Channel_Enhancement.py
+++ models/Channel_Enhancement.py
@@ -1,40 +1,32 @@
-"""latest version of SuperpointNet. Use it!
 
-"""
 import sys, os
 
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(base_path)
 import torch
 
-from models.unet_parts import *
-from models.dcd import conv_basic_dy
-from models.ea import External_attention
-# from models.SubpixelNet import SubpixelNet
 from models.ECA import ECAAttention
 from models.rep import RepVGGBlock, repvgg_model_convert
 
-class SuperPointNet_dcd(torch.nn.Module):
-    """ Pytorch definition of SuperPoint Network. """
+class Channel_Enhancement(torch.nn.Module):
+
     def __init__(self, deploy=False):
-        super(SuperPointNet_dcd, self).__init__()
+        super(Channel_Enhancement, self).__init__()
         c1, c2, c3, c4, c5, d1 = 32, 64, 128, 256, 64, 256
         det_h = 65
-        # c1, c2, c3, c4, c5, d1 = 32, 64, 128, 128, 256, 256
-        # det_h = 65
+
         self.deploy = deploy
         self.relu = torch.nn.ReLU(inplace=True)
-        # self.outc = outconv(64, n_classes)
 
         self.pool = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        # self.conv1 = torch.nn.Conv2d(1, c1, kernel_size=3, stride=1, padding=1)
+
         self.conv1 = RepVGGBlock(1, c1, kernel_size=3, stride=2, padding=1, deploy=self.deploy, use_se=False)
-        # self.bn1 = nn.BatchNorm2d(c1)
+
         self.conv2 = RepVGGBlock(c1, c1, kernel_size=3, stride=1, padding=1, deploy=self.deploy, use_se=False)
-        # self.bn2 = nn.BatchNorm2d(c2)
+
         self.conv3 = RepVGGBlock(c1, c2, kernel_size=3, stride=2, padding=1, deploy=self.deploy, use_se=False)
-        # self.bn3 = nn.BatchNorm2d(c3)
+
         self.conv4 = RepVGGBlock(c2, c2, kernel_size=3, stride=1, padding=1, deploy=self.deploy, use_se=False)
 
         self.conv5 = RepVGGBlock(c2, c3, kernel_size=3, stride=2, padding=1, deploy=self.deploy, use_se=False)
@@ -52,12 +44,11 @@ class SuperPointNet_dcd(torch.nn.Module):
         self.convS_2 = torch.nn.Conv2d(c5, d1, kernel_size=1, stride=1, padding=0)
         self.bnS_2 = nn.BatchNorm2d(d1)
 
-        # Detector Head.
         self.convDF_1 = torch.nn.Conv2d(c5, c5, kernel_size=3, stride=1, padding=1)
         self.bnDF_1 = nn.BatchNorm2d(c5)
         self.convDF_2 = torch.nn.Conv2d(c5, det_h, kernel_size=1, stride=1, padding=0)
         self.bnDF_2 = nn.BatchNorm2d(det_h)
-        # Descriptor Head.
+
         self.convDD_1 = torch.nn.Conv2d(c5, d1, kernel_size=3, stride=1, padding=1)
         self.bnDD_1 = nn.BatchNorm2d(d1)
         self.convDD_2 = torch.nn.Conv2d(d1, d1, kernel_size=1, stride=1, padding=0)
@@ -65,15 +56,7 @@ class SuperPointNet_dcd(torch.nn.Module):
 
 
     def forward(self, x):
-        """ Forward pass that jointly computes unprocessed point and descriptor
-        tensors.
-        Input
-          x: Image pytorch tensor shaped N x 1 x patch_size x patch_size.
-        Output
-          semi: Output point pytorch tensor shaped N x 65 x H/8 x W/8.
-          desc: Output descriptor pytorch tensor shaped N x 256 x H/8 x W/8.
-        """
-        # Let's stick to this version: first BN, then relu
+
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
@@ -90,15 +73,11 @@ class SuperPointNet_dcd(torch.nn.Module):
 
         x = self.relu(self.conv_attention(x))
 
-        # Detector Head.
         Head_DF = self.relu(self.bnDF_1(self.convDF_1(x)))
-        # cPa = cPa + x_1  # 升维前融合
         semi = self.bnDF_2(self.convDF_2(Head_DF))
         semi = semi + x_1
 
-        # Descriptor Head.
         Head_DD = self.relu(self.bnDD_1(self.convDD_1(x)))
-        # cDa = cDa + x_2  # 升维前融合
         desc = self.bnDD_2(self.convDD_2(Head_DD))
         desc = desc + x_2
         dn = torch.norm(desc, p=2, dim=1) # Compute the norm.
@@ -108,12 +87,11 @@ class SuperPointNet_dcd(torch.nn.Module):
 
 
 
-
 def main(weights_path, save_path):
   
   print('==> Loading pre-trained network.')
   # This class runs the SuperPoint network and processes its outputs.
-  net = SuperPointNet_dcd()
+  net = Channel_Enhancement()
   checkpoint = torch.load(weights_path, map_location=lambda storage, loc: storage)
   net.load_state_dict(checkpoint['model_state_dict'])
   deploy_net = repvgg_model_convert(net, save_path=save_path)
@@ -121,4 +99,4 @@ def main(weights_path, save_path):
   return deploy_net
 
 if __name__ == '__main__':
-    main('dcd.pth.tar','./weight/dcd_weight')
+    main('CE.pth.tar','./weight/CE_weight')
